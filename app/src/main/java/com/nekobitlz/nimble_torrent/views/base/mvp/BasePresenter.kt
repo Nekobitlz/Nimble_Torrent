50889371cbe69c6ef98c875d073388246fcfd97b
Nekobitlz <jokers.bit@gmail.com>
2019-12-21 21:17:37 +0300

    Change Disposable to Subscription and add BaseSubscriber interface

--- app/src/main/java/com/nekobitlz/nimble_torrent/views/base/mvp/BasePresenter.kt
+++ app/src/main/java/com/nekobitlz/nimble_torrent/views/base/mvp/BasePresenter.kt
@@ -1,24 +1,42 @@
 package com.nekobitlz.nimble_torrent.views.base.mvp
 
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
+import rx.Subscriber
+import rx.Subscription
+import rx.subscriptions.CompositeSubscription
 
 open class BasePresenter<V: BaseContract.View> : BaseContract.Presenter<V> {
 
     lateinit var view: V
         private set
 
-    private val disposables = CompositeDisposable()
+    private val subscription = CompositeSubscription()
 
     override fun attachView(view: V) {
         this.view = view
     }
 
     override fun detachView() {
-        disposables.clear()
+        subscription.unsubscribe()
     }
 
-    fun Disposable.addDisposables() {
-        disposables.add(this)
+    fun Subscription.addSubscription() {
+        subscription.add(this)
+    }
+
+    abstract inner class BaseSubscriber<V>(val isLoading: Boolean = true) : Subscriber<V>() {
+        override fun onCompleted() {
+            if (isLoading) {
+                view.setLoading(false)
+            }
+        }
+
+        override fun onStart() {
+            if (isLoading) view.setLoading(true)
+            super.onStart()
+        }
+
+        override fun onError(e: Throwable?) {
+            e?.printStackTrace()
+        }
     }
 }
