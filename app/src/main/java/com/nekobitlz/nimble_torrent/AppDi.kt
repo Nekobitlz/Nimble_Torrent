b3e858fae3b5d45642f29d28bfb05079583e8de0
Nekobitlz <jokers.bit@gmail.com>
2019-12-21 23:46:48 +0300

    Add repository and database providing to AppDi

--- app/src/main/java/com/nekobitlz/nimble_torrent/AppDi.kt
+++ app/src/main/java/com/nekobitlz/nimble_torrent/AppDi.kt
@@ -1,27 +1,66 @@
 package com.nekobitlz.nimble_torrent
 
+import android.app.Application
 import android.content.Context
-import com.nekobitlz.nimble_torrent.views.base.di.AppContext
-import dagger.Binds
+import androidx.room.Room
+import com.nekobitlz.nimble_torrent.repository.ITorrentRepository
+import com.nekobitlz.nimble_torrent.repository.TorrentRepository
+import com.nekobitlz.nimble_torrent.repository.database.TorrentDao
+import com.nekobitlz.nimble_torrent.repository.database.TorrentDatabase
+import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
+import dagger.Provides
 import dagger.android.AndroidInjectionModule
-import dagger.android.AndroidInjector
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
-interface AppComponent : AndroidInjector<NimbleApplication> {
+@Component(modules = [AndroidInjectionModule::class, AppModule::class, TorrentRepositoryModule::class, DatabaseModule::class])
+interface AppComponent {
 
-    @Component.Factory
-    abstract class Builder : AndroidInjector.Factory<NimbleApplication>
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(application: NimbleApplication)
+    fun getTorrentRepository(): ITorrentRepository
 }
 
-@Module(includes = [AndroidInjectionModule::class])
-abstract class AppModule {
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context = application
+}
+
+
+@Module
+class TorrentRepositoryModule {
+
+    @Provides
+    @Singleton
+    fun providesTorrentRepository(dao: TorrentDao): ITorrentRepository {
+        return TorrentRepository(dao)
+    }
+}
+
+@Module
+class DatabaseModule {
+
+    private val DB_NAME = "torrent_db"
 
     @Singleton
-    @Binds
-    @AppContext
-    abstract fun provideContext(application: NimbleApplication): Context
+    @Provides
+    fun provideTorrentDatabase(context: Context): TorrentDatabase {
+        return Room.databaseBuilder(context, TorrentDatabase::class.java, DB_NAME).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTorrentDao(db: TorrentDatabase): TorrentDao = db.torrentDao()
 }
