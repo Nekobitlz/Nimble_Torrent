5ac3aab1661f1596a75da2f0892d4eb3dae5faa1
Nekobitlz <jokers.bit@gmail.com>
2019-12-28 06:08:06 +0300

    Add a dummy for the torrents browse screen

--- app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/torrentbrowse/TorrentBrowseFragment.kt
+++ app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/torrentbrowse/TorrentBrowseFragment.kt
@@ -1,6 +1,11 @@
 package com.nekobitlz.nimble_torrent.views.fragments.torrentbrowse
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nekobitlz.nimble_torrent.R
 import com.nekobitlz.nimble_torrent.views.base.mvp.BaseFragment
 
 class TorrentBrowseFragment : BaseFragment() {
@@ -13,4 +18,10 @@ class TorrentBrowseFragment : BaseFragment() {
             return torrentBrowseFragment
         }
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_browse, container, false)
 }
