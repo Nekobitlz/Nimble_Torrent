2d6dca6ff7baa10f63b5d82a8435cdb2190e23b3
Nekobitlz <jokers.bit@gmail.com>
2020-01-03 21:52:56 +0300

    Make the download fragment display only the downloaded data

--- app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/downloads/DownloadsContract.kt
+++ app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/downloads/DownloadsContract.kt
@@ -9,13 +9,11 @@ interface DownloadsContract {
     interface View: BaseContract.View {
         fun showToast(text: String)
         fun setupView(torrentFiles: List<TorrentData>)
-    //    fun updateTorrentView(torrentFile: TorrentStatus)
     }
 
     interface Presenter: BaseContract.Presenter<View> {
         fun onSetup(arguments: Bundle?)
         fun onRefresh()
         fun onDeleteClick(torrentData: TorrentData)
-        //   fun onViewClicked(torrentFile: TorrentFile, action: ClickType)
     }
 }
--- app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/downloads/DownloadsPresenter.kt
+++ app/src/main/java/com/nekobitlz/nimble_torrent/views/fragments/downloads/DownloadsPresenter.kt
@@ -1,9 +1,6 @@
 package com.nekobitlz.nimble_torrent.views.fragments.downloads
 
 import android.os.Bundle
-import com.github.se_bastiaan.torrentstream.StreamStatus
-import com.github.se_bastiaan.torrentstream.Torrent
-import com.github.se_bastiaan.torrentstream.listeners.TorrentListener
 import com.nekobitlz.nimble_torrent.repository.ITorrentRepository
 import com.nekobitlz.nimble_torrent.repository.database.TorrentData
 import com.nekobitlz.nimble_torrent.views.base.mvp.BasePresenter
@@ -11,30 +8,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class DownloadsPresenter(private val torrentRepository: ITorrentRepository) :
-    BasePresenter<DownloadsContract.View>(), DownloadsContract.Presenter, TorrentListener {
-    override fun onStreamReady(torrent: Torrent) {
-        view.showToast("Ready")
-    }
-
-    override fun onStreamPrepared(torrent: Torrent) {
-        view.showToast("Prepared")
-    }
-
-    override fun onStreamStopped() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onStreamStarted(torrent: Torrent?) {
-        view.showToast("Started")
-    }
-
-    override fun onStreamProgress(torrent: Torrent?, status: StreamStatus?) {
-        view.showToast(status.toString())
-    }
-
-    override fun onStreamError(torrent: Torrent?, e: Exception?) {
-        view.showToast(e.toString())
-    }
+    BasePresenter<DownloadsContract.View>(), DownloadsContract.Presenter {
 
     override fun onSetup(arguments: Bundle?) {
         torrentRepository.getDownloadingFiles()
